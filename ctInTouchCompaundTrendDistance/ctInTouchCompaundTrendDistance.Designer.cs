649f37abdccd6502dc15fa3888200f43950ebe3f
Александр Макаров <makarovas82@gmail.com>
2021-01-25 15:14:08 +0400

    Изменил скорость

---
+++ ctInTouchCompaundTrendDistance/ctInTouchCompaundTrendDistance.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace ctInTouchCompaundTrend
+{
+    partial class ctInTouchCompaundTrendDistance
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.шкалаЗначенийToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.автоToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.l_cursor = new System.Windows.Forms.Label();
+            this.contextMenuStrip2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.шкалаЗначенийToolStripMenuItem});
+            this.contextMenuStrip2.Name = "contextMenuStrip1";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(176, 48);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.CheckOnClick = true;
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(175, 22);
+            this.toolStripMenuItem1.Text = "Показывать метки";
+            this.toolStripMenuItem1.CheckedChanged += new System.EventHandler(this.toolStripMenuItem1_CheckedChanged);
+            // 
+            // шкалаЗначенийToolStripMenuItem
+            // 
+            this.шкалаЗначенийToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem3,
+            this.автоToolStripMenuItem});
+            this.шкалаЗначенийToolStripMenuItem.Name = "шкалаЗначенийToolStripMenuItem";
+            this.шкалаЗначенийToolStripMenuItem.Size = new System.Drawing.Size(175, 22);
+            this.шкалаЗначенийToolStripMenuItem.Text = "Шкала значений";
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Checked = true;
+            this.toolStripMenuItem3.CheckOnClick = true;
+            this.toolStripMenuItem3.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(105, 22);
+            this.toolStripMenuItem3.Text = "0..5 %";
+            this.toolStripMenuItem3.CheckedChanged += new System.EventHandler(this.toolStripMenuItem3_CheckedChanged);
+            this.toolStripMenuItem3.Click += new System.EventHandler(this.toolStripMenuItem3_Click);
+            // 
+            // автоToolStripMenuItem
+            // 
+            this.автоToolStripMenuItem.CheckOnClick = true;
+            this.автоToolStripMenuItem.Name = "автоToolStripMenuItem";
+            this.автоToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
+            this.автоToolStripMenuItem.Text = "Авто";
+            this.автоToolStripMenuItem.CheckedChanged += new System.EventHandler(this.автоToolStripMenuItem_CheckedChanged);
+            this.автоToolStripMenuItem.Click += new System.EventHandler(this.автоToolStripMenuItem_Click);
+            // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.Color.Transparent;
+            chartArea1.AxisX.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
+            chartArea1.AxisX.IsLabelAutoFit = false;
+            chartArea1.AxisX.LabelAutoFitMaxFontSize = 8;
+            chartArea1.AxisX.LabelStyle.Format = "{0:f3}";
+            chartArea1.AxisX.LineWidth = 0;
+            chartArea1.AxisX.MajorGrid.Enabled = false;
+            chartArea1.AxisX.MajorGrid.LineColor = System.Drawing.Color.LightGray;
+            chartArea1.AxisX.MajorTickMark.Enabled = false;
+            chartArea1.AxisX.Minimum = 0D;
+            chartArea1.AxisX.MinorGrid.LineColor = System.Drawing.Color.Gainsboro;
+            chartArea1.AxisX.ScaleView.Zoomable = false;
+            chartArea1.AxisX.ScrollBar.Enabled = false;
+            chartArea1.AxisX.ScrollBar.IsPositionedInside = false;
+            chartArea1.AxisX2.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.False;
+            chartArea1.AxisY.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
+            chartArea1.AxisY.IsLabelAutoFit = false;
+            chartArea1.AxisY.LabelStyle.Format = "{0:f3}";
+            chartArea1.AxisY.LabelStyle.Interval = 0D;
+            chartArea1.AxisY.LabelStyle.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Auto;
+            chartArea1.AxisY.LineWidth = 0;
+            chartArea1.AxisY.MajorGrid.Enabled = false;
+            chartArea1.AxisY.MajorGrid.LineColor = System.Drawing.Color.LightGray;
+            chartArea1.AxisY.MajorTickMark.Enabled = false;
+            chartArea1.AxisY.MajorTickMark.LineColor = System.Drawing.Color.LightGray;
+            chartArea1.AxisY.MinorGrid.Interval = 0.5D;
+            chartArea1.AxisY.MinorGrid.LineColor = System.Drawing.Color.Gainsboro;
+            chartArea1.AxisY2.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.False;
+            chartArea1.BorderWidth = 0;
+            chartArea1.CursorX.Interval = 0.1D;
+            chartArea1.CursorX.IsUserEnabled = true;
+            chartArea1.CursorX.LineColor = System.Drawing.Color.Blue;
+            chartArea1.InnerPlotPosition.Auto = false;
+            chartArea1.InnerPlotPosition.Height = 100F;
+            chartArea1.InnerPlotPosition.Width = 100F;
+            chartArea1.Name = "ChartArea1";
+            chartArea1.Position.Auto = false;
+            chartArea1.Position.Height = 100F;
+            chartArea1.Position.Width = 100F;
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.ContextMenuStrip = this.contextMenuStrip2;
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chart1.Location = new System.Drawing.Point(0, 0);
+            this.chart1.Margin = new System.Windows.Forms.Padding(0);
+            this.chart1.Name = "chart1";
+            series1.BorderWidth = 4;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Area;
+            series1.LabelFormat = "{0:f3}";
+            series1.LabelToolTip = "#VALX";
+            series1.Legend = "Legend1";
+            series1.MarkerColor = System.Drawing.Color.Navy;
+            series1.MarkerSize = 2;
+            series1.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Square;
+            series1.Name = "Series1";
+            series1.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Single;
+            series1.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Single;
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(1200, 110);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart2";
+            this.chart1.CursorPositionChanged += new System.EventHandler<System.Windows.Forms.DataVisualization.Charting.CursorEventArgs>(this.chart2_CursorPositionChanged);
+            this.chart1.Click += new System.EventHandler(this.chart2_Click);
+            this.chart1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.chart2_KeyDown);
+            this.chart1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.chart1_MouseMove);
+            // 
+            // l_cursor
+            // 
+            this.l_cursor.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.l_cursor.BackColor = System.Drawing.Color.Blue;
+            this.l_cursor.ForeColor = System.Drawing.Color.White;
+            this.l_cursor.Location = new System.Drawing.Point(204, 81);
+            this.l_cursor.Name = "l_cursor";
+            this.l_cursor.Size = new System.Drawing.Size(112, 29);
+            this.l_cursor.TabIndex = 3;
+            this.l_cursor.Text = "#.## % ###.## км\r\n##.##.## ##:##:##";
+            this.l_cursor.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.l_cursor.Visible = false;
+            // 
+            // ctInTouchCompaundTrendDistance
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.l_cursor);
+            this.Controls.Add(this.chart1);
+            this.Name = "ctInTouchCompaundTrendDistance";
+            this.Size = new System.Drawing.Size(1200, 110);
+            this.contextMenuStrip2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem шкалаЗначенийToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem автоToolStripMenuItem;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label l_cursor;
+    }
+}
---
+++ ctInTouchCompaundTrendDistance/ctInTouchCompaundTrendDistance.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ctInTouchCompaundTrend
+{
+    public partial class ctInTouchCompaundTrendDistance : UserControl
+    {
+        private float _PipeL;
+        private float _PipeD;
+        private float _Vcur;
+
+        public event EventHandler SpeedChanged;
+
+        public ctInTouchCompaundTrendDistance()
+        {
+            InitializeComponent();
+
+            //SpeedChanged += onSpeedChanged;
+
+#if DEBUG
+
+            PipeD = Convert.ToSingle(0.8);
+            PipeL = Convert.ToSingle(100000);
+            Vcur = Convert.ToSingle(1543);
+
+            var rand = new Random();
+            for (int i = 0; i < 1000; i++)
+            {
+                LoadData(Convert.ToSingle(1.3 + (rand.Next(50, 130) / 100.0)), (i * 60 * 1000), (1000 - i) * 100);
+            }
+
+#endif
+        }
+
+        public Color Chart_Backcolor
+        {
+            set
+            {
+                chart1.ChartAreas[0].BackColor = value;
+            }
+            get
+            {
+                return chart1.ChartAreas[0].BackColor;
+            }
+        }
+
+        public double axisX_Max
+        {
+            get
+            {
+                return chart1.ChartAreas[0].AxisX.Maximum;
+            }
+        }
+
+        public double axisX_Min
+        {
+            get
+            {
+                return chart1.ChartAreas[0].AxisX.Minimum;
+            }
+        }
+
+        public double axisY_Max
+        {
+            get
+            {
+                return chart1.ChartAreas[0].AxisY.Maximum;
+            }
+        }
+
+        public double axisY_Min
+        {
+            get
+            {
+                return chart1.ChartAreas[0].AxisY.Minimum;
+            }
+        }
+
+        public void Clear()
+        {
+            chart1.Series[0].Points.Clear();
+            UpdateCursor_Dist(chart1, l_cursor, Double.NaN);
+        }
+
+        public void LoadData(float SOil, float Delay, float Dist)
+        {
+            try
+            {
+                chart1.Series[0].Points[chart1.Series[0].Points.AddXY(Dist / 1000.0, SOil)].Tag = DateTime.Now.AddMilliseconds(Delay);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        public float PipeL
+        {
+            get
+            {
+                return _PipeL;
+            }
+            set
+            {
+                _PipeL = value;
+                SpeedChanged?.Invoke(this, null);
+                try
+                {
+                    chart1.ChartAreas[0].AxisX.Maximum = _PipeL / 1000.0;
+                }
+                catch { }
+            }
+        }
+
+        public float PipeD
+        {
+            get
+            {
+                return _PipeD;
+            }
+            set
+            {
+                _PipeD = value;
+                SpeedChanged?.Invoke(this, null);
+            }
+        }
+
+        public float Vcur
+        {
+            get
+            {
+                return _Vcur;
+            }
+            set
+            {
+                _Vcur = value;
+                SpeedChanged?.Invoke(this, null);
+            }
+        }
+
+        public float Speed
+        {
+            get
+            {
+                try
+                {
+                    return (4 * Vcur) / 3600 * (float)Math.PI * PipeD * PipeD;
+                }
+                catch (DivideByZeroException e)
+                {
+                    return -1000;
+                }
+            }
+        }
+
+        private void UpdateCursor_Dist(System.Windows.Forms.DataVisualization.Charting.Chart chart, Label label, double position)
+        {
+            System.Windows.Forms.DataVisualization.Charting.DataPoint pPrev;
+            System.Windows.Forms.DataVisualization.Charting.DataPoint pNext;
+            try
+            {
+                label.Visible = !Double.IsNaN(position) && (chart.Series[0].Points.Count > 0);
+
+                pPrev = chart.Series[0].Points.Select(x => x)
+                    .Where(x => x.XValue >= position)
+                    .DefaultIfEmpty(chart.Series[0].Points.Last())
+                    .Last();
+
+                pNext = chart.Series[0].Points.Select(x => x)
+                    .Where(x => x.XValue <= position)
+                    .DefaultIfEmpty(chart.Series[0].Points.First())
+                    .First();
+
+                pNext = (position - pPrev.XValue > pNext.XValue - position) ? pPrev : pNext;
+
+                label.Text = String.Format("{0:f2}% {1:f2}км.\n{2}", pNext.YValues[0], position, pNext.Tag.ToString());
+                label.Location = new Point((int)chart.ChartAreas[0].AxisX.ValueToPixelPosition(position) - (chart.ChartAreas[0].AxisX.ValueToPosition(position) < 50 ? 0 : label.Size.Width), chart1.Height - label.Size.Height);
+            }
+            catch (Exception ex)
+            {
+            }
+            label.Refresh();
+
+            System.Threading.Thread.Sleep(1);
+        }
+
+        private void chart2_AxisViewChanged(object sender, System.Windows.Forms.DataVisualization.Charting.ViewEventArgs e)
+        {
+        }
+
+        private void toolStripMenuItem1_CheckedChanged(object sender, EventArgs e)
+        {
+            chart1.Series[0].IsValueShownAsLabel = toolStripMenuItem1.Checked;
+        }
+
+        private void chart2_Paint(object sender, PaintEventArgs e)
+        {
+        }
+
+        private void chart2_CursorPositionChanged(object sender, System.Windows.Forms.DataVisualization.Charting.CursorEventArgs e)
+        {
+            UpdateCursor_Dist(chart1, l_cursor, e.NewPosition);
+        }
+
+        private void chart2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+        }
+
+        private void chart2_Click(object sender, EventArgs e)
+        {
+            chart1.Focus();
+            UpdateCursor_Dist(chart1, l_cursor, chart1.ChartAreas[0].CursorX.Position);
+        }
+
+        private void chart2_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+        }
+
+        private void автоToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void автоToolStripMenuItem_CheckedChanged(object sender, EventArgs e)
+        {
+            if (автоToolStripMenuItem.Checked)
+            {
+                chart1.ChartAreas[0].AxisY.Minimum = double.NaN;
+                chart1.ChartAreas[0].AxisY.Maximum = double.NaN;
+            }
+
+            toolStripMenuItem3.Checked = !автоToolStripMenuItem.Checked;
+        }
+
+        private void toolStripMenuItem3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (автоToolStripMenuItem.Checked)
+            {
+                chart1.ChartAreas[0].AxisY.Minimum = 0.0;
+                chart1.ChartAreas[0].AxisY.Maximum = 5.0;
+            }
+
+            автоToolStripMenuItem.Checked = !toolStripMenuItem3.Checked;
+        }
+
+        private void toolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void chart1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+        }
+
+        private void chart2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (!Double.IsNaN(chart1.ChartAreas[0].CursorX.Position))
+            {
+                if ((e.KeyCode == Keys.OemPeriod) | (e.KeyCode == Keys.Oemcomma))
+                {
+                    System.Windows.Forms.DataVisualization.Charting.DataPoint pPointer;
+
+                    try
+                    {
+                        l_cursor.Visible = !Double.IsNaN(chart1.ChartAreas[0].CursorX.Position);
+
+                        if ((e.KeyCode == Keys.Oemcomma))
+                            pPointer = chart1.Series[0].Points.Select(x => x)
+                                .Where(x => x.XValue < chart1.ChartAreas[0].CursorX.Position)
+                                .DefaultIfEmpty(chart1.Series[0].Points.First())
+                                .First();
+                        else
+                            pPointer = chart1.Series[0].Points.Select(x => x)
+                                .Where(x => x.XValue > chart1.ChartAreas[0].CursorX.Position)
+                                .DefaultIfEmpty(chart1.Series[0].Points.Last())
+                                .Last();
+
+                        chart1.ChartAreas[0].CursorX.Position = pPointer.XValue;
+                    }
+                    catch (Exception ex)
+                    {
+                    }
+                }
+            }
+            UpdateCursor_Dist(chart1, l_cursor, chart1.ChartAreas[0].CursorX.Position);
+        }
+
+        private void chart1_MouseMove(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                if (e.Button == MouseButtons.Left)
+                {
+                    chart1.ChartAreas[0].CursorX.Position = chart1.ChartAreas[0].AxisX.PixelPositionToValue(e.X);
+                    UpdateCursor_Dist(chart1, l_cursor, chart1.ChartAreas[0].CursorX.Position);
+                }
+            }
+            catch
+            {
+            }
+        }
+    }
+}
